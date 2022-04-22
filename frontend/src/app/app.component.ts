c3a4bf7b2c7ac457ca8641bec5bae3f91a042cc1
Thomas Sikora <thomas-sikora@web.de>
2022-04-22 15:57:09 +0200

    Adding Title, Logo & Favicon

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {PrimeNGConfig} from 'primeng/api';
+import {Title} from '@angular/platform-browser';
 
 @Component({
     selector: 'app-root',
@@ -7,14 +8,17 @@ import {PrimeNGConfig} from 'primeng/api';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-    title = 'frontend';
+
+    title = 'YAMP - yet another mobbing program';
 
     constructor(
-        private primengConfig: PrimeNGConfig
+        private primengConfig: PrimeNGConfig,
+        private titleService: Title
     ) {
     }
 
     ngOnInit() {
+        this.titleService.setTitle(this.title);
         this.primengConfig.ripple = true;
     }
 }
